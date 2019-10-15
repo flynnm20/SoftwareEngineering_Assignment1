5ba52b07ba58d733b0276b3635a34c69264dd5d1
flynnm20 <46966461+flynnm20@users.noreply.github.com>
2019-10-15 09:25:05 +0100

    Update dagTest.java

--- dagTest.java
+++ dagTest.java
@@ -4,16 +4,29 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class dagTest {
-
-	BST<Integer, Integer> bst = new BST<Integer,Integer>();
-    @Test
-    public void testNull(){
-        assertNull("Testing the Null case", bst.lowestCommonAncestor(2,3));
-    }
-    @Test
-    public void testOneNode(){
-        bst.insert(1,1);
-        assertNull("Testing the One Node case", bst.lowestCommonAncestor(2,3));
-    }
+	
+    dag testDag = new dag(5);
+	@Test
+	public void testDAGLowestCommonAncestor() {
+	testDag.addEdge(0,1);
+	testDag.addEdge(0,2);
+	testDag.addEdge(2,3);
+	testDag.addEdge(3,4);
+	int expectedAncestor = 0;
+	int result = testDag.lowestCommonAncestor(1,2).get(0);
+    assertEquals("Checking basic answer",expectedAncestor,result);
+    expectedAncestor = 3;
+	result = testDag.lowestCommonAncestor(4,3).get(0);
+    assertEquals("Checking answer for the same tree",expectedAncestor,result);
+    
+    testDag.addEdge(4,1); // Makes the graph a directed circle.
+    expectedAncestor = 4;
+	result = testDag.lowestCommonAncestor(1,4).get(0);
+	System.out.print(result);
+    assertEquals("Checking answer for the same tree",expectedAncestor,result);
+    
+    testDag=null; // reset for new tests
+    
+	}
 
 }
