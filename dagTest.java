091d815c475ed55c1e703e5448b24bbc5c76786b
flynnm20 <46966461+flynnm20@users.noreply.github.com>
2019-10-16 09:30:11 +0100

    Update dagTest.java

--- dagTest.java
+++ dagTest.java
@@ -1,32 +1,40 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
 
 class dagTest {
-	
-    dag testDag = new dag(5);
+
+	dag testDag = new dag(5);
+	dag testOne = new dag(1);
 	@Test
 	public void testDAGLowestCommonAncestor() {
-	testDag.addEdge(0,1);
-	testDag.addEdge(0,2);
-	testDag.addEdge(2,3);
-	testDag.addEdge(3,4);
-	int expectedAncestor = 0;
-	int result = testDag.lowestCommonAncestor(1,2).get(0);
-    assertEquals("Checking basic answer",expectedAncestor,result);
-    expectedAncestor = 3;
-	result = testDag.lowestCommonAncestor(4,3).get(0);
-    assertEquals("Checking answer for the same tree",expectedAncestor,result);
-    
-    testDag.addEdge(4,1); // Makes the graph a directed circle.
-    expectedAncestor = 4;
-	result = testDag.lowestCommonAncestor(1,4).get(0);
-	System.out.print(result);
-    assertEquals("Checking answer for the same tree",expectedAncestor,result);
-    
-    testDag=null; // reset for new tests
-    
+		assertTrue(testDag.lowestCommonAncestor(0, 0).isEmpty());
+		assertTrue(testOne.lowestCommonAncestor(0, 0).isEmpty());
+		testDag.addEdge(0,1);
+		testDag.addEdge(0,2);
+		testDag.addEdge(2,3);
+		testDag.addEdge(3,4);
+		int expectedAncestor = 0;
+		int result = testDag.lowestCommonAncestor(1,2).get(0);
+	    assertEquals("Checking basic answer",expectedAncestor,result);
+	    expectedAncestor = 3;
+		result = testDag.lowestCommonAncestor(4,3).get(0);
+	    assertEquals("Checking answer for the same tree",expectedAncestor,result);
+	    
+	    testDag.addEdge(4,1); // Makes the graph a directed circle.
+	    expectedAncestor = 4;
+		result = testDag.lowestCommonAncestor(1,4).get(0);
+		System.out.print(result);
+	    assertEquals("Checking answer for the same tree",expectedAncestor,result);
+	    
+	    testDag=null; // reset for new tests
+	    
 	}
-
+	
 }
